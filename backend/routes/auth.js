f54706f5c6ebb658c4f42125e13ce8fe5e63e590
amisha <amishaadwani0023@gmail.com>
2023-07-30 22:20:00 +0530

    error handling and comments added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,25 +1,39 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
-const {body, validationResult}= require('express-validator')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post('/',[
-    body('name').isLength({min: 3}).withMessage('Not a valid name'),
-    body('email').isEmail().withMessage('Not a valid email'),
-    body('password').isLength({min:5}).withMessage('Not a valid password')
-],(req,res)=>{
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors :errors.array()})
+router.post(
+  "/",
+  //validation using express-validator
+  [
+    body("name").isLength({ min: 3 }).withMessage("Not a valid name"),
+    body("email").isEmail().withMessage("Not a valid email"),
+    body("password").isLength({ min: 5 }).withMessage("Not a valid password"),
+  ],
+  //validation response if error occurs
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    try {
+      // create user
+      User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err)
-    res.json({error: 'Please enter a unique value'})} )
-})
+        password: req.body.password,
+      })
+        .then((user) => res.json(user)) //send user as response
+        .catch((err) => {
+          console.log(err);
+          res.json({ error: "Please enter a unique value" });
+        });
+    } catch (error) {
+      res.status(500).send("Error occured");
+    }
+  }
+);
 // res.json(req.body)
 
-module.exports = router
+module.exports = router;
