ecb8f39c813b753aedc7a31ded2dcb504549daba
amisha <amishaadwani0023@gmail.com>
2023-08-01 23:43:12 +0530

    hashPassword using bcryptjs added

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = require('./routes/auth')
 main(); 
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(express.json())
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-
+const bcrypt = require('bcryptjs')
 router.post(
   "/",
   //validation using express-validator
@@ -12,17 +12,19 @@ router.post(
     body("password").isLength({ min: 5 }).withMessage("Not a valid password"),
   ],
   //validation response if error occurs
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     try {
+      const salt = await bcrypt.genSalt(10)
+      const securedPass = await bcrypt.hash(req.body.password, salt)
       // create user
-      User.create({
+     await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: securedPass,
       })
         .then((user) => res.json(user)) //send user as response
         .catch((err) => {
