a5985534d0c30690c0d55269d42f3c4764a290be
amisha <amishaadwani0023@gmail.com>
2023-08-08 20:32:02 +0530

    setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavbarBrand } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import {LinkContainer} from 'react-router-bootstrap';
+
+const NavBar = () => {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <LinkContainer to ="/home">
+            <Navbar.Brand>iNote</Navbar.Brand>
+          </LinkContainer>
+          <Nav className="me-auto">
+          <LinkContainer to ="/home">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to ="/about">
+            <Nav.Link>About</Nav.Link>
+          </LinkContainer>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;
